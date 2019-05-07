164cbbf0b03bb715842c1efc13d601ce563fadab
mtruong02 <48307478+mtruong02@users.noreply.github.com>
2019-05-07 13:06:05 -0400

    [Fix] Space bar button added

--- APCSFinalProject/src/GameListeners.java
+++ APCSFinalProject/src/GameListeners.java
@@ -10,27 +10,33 @@ public class GameListeners extends KeyAdapter {
 		this.handler = handler;
 	}
 	
+	/*
+	 * keyPressed method
+	 * takes in user input
+	 * each button performs a certain action
+	 * (Minh Truong)
+	 */
 	public void keyPressed(KeyEvent e) {
 		for (int i = 0; i < handler.objects.size(); i++) {
 			if (handler.objects.get(i).getIsEnemy() == false) {
 				GameObject p = handler.objects.get(i);
-				if (e.getKeyCode() == KeyEvent.VK_UP) {
+				if (e.getKeyCode() == KeyEvent.VK_UP) {	//up arrow makes the object go up
 				p.setSpeedY(-6);
 					
 				}
-				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				if (e.getKeyCode() == KeyEvent.VK_DOWN) {	//down arrow makes the object go down
 					p.setSpeedY(6);
 					
 				}
-				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {	//left arrow makes object go left
 					p.setSpeedX(-6);
 			
 				}
-				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {	
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {	//right arrow makes object go right
 					p.setSpeedX(6);
 
 				}
-				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {	//space bar restarts the game
 					
 					handler.restart();
 					handler.state = true;
@@ -42,10 +48,17 @@ public class GameListeners extends KeyAdapter {
 		}	
 		}
 	
+	/*
+	 * keyReleased method
+	 * takes in user input
+	 * each button performs a certain action
+	 * (Minh Truong)
+	 */
 	public void keyReleased(KeyEvent e) {
 		for (int i = 0; i < handler.objects.size(); i++) {
 			if (handler.objects.get(i).getIsEnemy() == false) {
 				GameObject p = handler.objects.get(i);
+				//sets the x and y positions back to 0
 				if (e.getKeyCode() == KeyEvent.VK_UP) {
 				p.setSpeedY(0);
 					
